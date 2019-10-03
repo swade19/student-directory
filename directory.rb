20607a646a925cad51159ab7dda55636c2051c09
Student <student@Admins-MacBook-Air.local>
2019-10-03 16:46:42 +0100

    whileloop

--- directory.rb
+++ directory.rb
@@ -1,43 +1,44 @@
-#first we print the list of students
-#students = [
-# ["Dr. Hannibal Lecter", :november],
- #["Darth Vader", :november],
- #[{}"Nurse Ratched", :november],
- #["Michael Corleone", :november],
- #["Alex DeLarge", :november],
- #["The Wicked Witch of the West", :november],
- #["Terminator", :november],
- #["Freddy Krueger", :november],
- #["The Joker", :november],
- #["Joffrey Baratheon", :november],
- #["Norman Bates", :november]
-#]
- # and then print them
- def input_students
-     puts "Please enter the names of the students"
-     puts "To finish, just hit return twice."
 
-     students = []
 
-     name = gets.chomp
-     while !name.empty? do
-         students << {Name: name, Cohort: :November}
-         puts "Now we have #{students.count} students"
-         name = gets.chomp
-     end
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice."
+    
+    students =[]
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+end 
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "______________________"
+end
 
 
 
-     students.each.with_index(1) do |hash| hash.each do | key, value|
-            if key.start_with?("A")
-              puts "#{key} #{value}"
-            end
-          end
+def index(students, letter)
+    i = 0
+    while students.count > i do
+        if (students[i][:name].length < 12)
+            puts "#{i}: #{students[i][:name]} #{students[i][:cohort]}"
         end
-      end
+        i +=1
+    end
+end
+
+def print_footer(names)
+      puts "Overall we have #{names.count} great students"
+end
+
+
+
+students = input_students
+print_header
+index(students, "f")
 input_students
 
-
-#finally, we print the total number of students
-#it's important that print () doesn't add new line characters
-# Update: trying to just print out students with A names.
--- test.rb
+++ test.rb
@@ -9,7 +9,7 @@ healthy = {
   "Aorange" => "made_up"
 }
 
-
+#grep is tricky with hash.... dont go there yet .
 
 result = healthy.grep(/^A/)
 puts healthy.grep(/^A/)
@@ -24,4 +24,12 @@ healthy.each do |key, value|
     end
 
 
+end
+
+students.each do | key, value |
+
+    if key.start_with?("A")
+        puts "#{key} #{value}"
+      end
+    end
 end
